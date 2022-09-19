139865761e7d73655bf2b029dce53b2693041832
dbarya <107024554+dbarya@users.noreply.github.com>
2022-09-19 08:50:19 +0700

    grading

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) =>{
+  try{
+    const cekEmosiTheaterIXX = await promiseTheaterIXX();
+    const cekEmosiTheaterVGC = await promiseTheaterVGC();
+
+    const cekHasilEmosiTheaterVGC = cekEmosiTheaterVGC.filter((res)=> res.hasil === emosi);
+    const cekHasilEmosiTheaterIXX = cekEmosiTheaterIXX.filter((res)=> res.hasil === emosi);
+    return cekHasilEmosiTheaterVGC.length + cekHasilEmosiTheaterIXX.length;
+  }
+catch(error) {}
+};
 
 module.exports = {
   promiseOutput,
